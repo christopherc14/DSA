e940d9bd64cb9a839c2752e7f7dd0c7664df16c4
christopherc14 <christopherc142003@gmail.com>
2025-10-25 11:36:16 +0530

    second

--- Divide and Conquer/mergesort.java
+++ Divide and Conquer/mergesort.java
@@ -1,69 +1,78 @@
 package DividenConquer;
 
-public class mergesort {
+public class mergesort{
 
-    public static void printArray(int arr[])
-    {
-        for(int i=0; i<arr.length; i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    }
-    
-    public static void mergeSort(int arr[], int start, int end)
+    public static void mergeSort(int nums[], int start, int end)
     {
         //Base Case
-        if(start>=end)
+        if(start >=end)
         {
             return;
         }
 
-        //Working
-        int mid= start+(end-start)/2;
-        mergeSort(arr, start, mid); //Left part
-        mergeSort(arr,mid+1, end);//Right Part
-        merge(arr,start, mid, end);
+        //Calculate mid value
+        int mid= start + (end - start) /2;
+        //Left part
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, start, mid, end);
     }
 
-    public static void merge(int arr[], int start, int mid, int end){
-    int temp[] = new int[end-start+1];
-    int i = start; //Iterator for left part
-    int j= mid+1; // Iterator for right part
-    int k = 0; //Iterator for temp array
+    public static void merge(int nums[], int start, int mid, int end)
+    {
+        int temp[] = new int[end - start +1];
+        int i=start; //Iterator to left part
+        int j= mid+1; //Iterator for right part
+        int k=0; //Iterator for temp array;
 
-    while(i<=mid && j<=end){ 
-        if(arr[i] < arr[j]){
-            temp[k] = arr[i];
-            i++; 
-        }else{
-            temp[k]= arr[j];
-            j++; 
+        while(i<=mid && j<=end)
+        {
+            if(nums[i] <= nums[j])
+            {
+                temp[k] = nums[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = nums[j];
+                j++;
+            }
+            k++;
         }
-        k++;
+
+        //Left part
+        while(i<=mid)
+        {
+            temp[k++] = nums[i++];
+        }
+
+        //Right part
+        while(j<=end)
+        {
+            temp[k++] = nums[j++];
+        }
+
+        //copy temp  to original
+        for(k=0, i=start; k<temp.length; k++, i++)
+        {
+            nums[i] = temp[k];
+        }
+
     }
 
-    //left part
-    while(i<=mid){
-        temp[k++]= arr[i++];
-    }
-
-    //Right part
-    while(j<=end)
+    public static void printarray(int nums[])
     {
-        temp[k++]= arr[j++];
+        for(int i=0; i<nums.length; i++)
+        {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
     }
 
-    //Copy temp to original array 
-    for(k=0, i=start; k<temp.length; k++, i++)
+    public static void main(String args[])
     {
-        arr[i]= temp[k];
-    }
-
- }
-    public static void main(String [] args){
-        int arr[] = {8,7,5,7,9,3,2,5,7,5,3,1};
-        mergeSort(arr, 0, arr.length-1);
-        printArray(arr);
+        int nums[] = {6,3,9,5,2,8};
+        mergeSort(nums, 0, nums.length-1);
+        printarray(nums);
     }
 }
